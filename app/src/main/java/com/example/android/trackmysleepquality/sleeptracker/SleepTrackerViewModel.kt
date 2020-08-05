f849c3e86c92f610287411c24680f508bab41ee6
Siwakorn <demon.evil.30@gmail.com>
2020-08-05 10:01:38 +0700

    6.1 Task: Add a ViewModel

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -25,6 +25,8 @@ import com.example.android.trackmysleepquality.database.SleepDatabaseDao
  */
 class SleepTrackerViewModel(
         val database: SleepDatabaseDao,
-        application: Application) : AndroidViewModel(application) {
+        application: Application)
+    : AndroidViewModel(application) {
+
 }
 
